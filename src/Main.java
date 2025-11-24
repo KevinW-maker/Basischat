3a15f8fc5b142d48d1ff62ff9cc0ebf49d70c182
Kevin <120616nestecs.drv>
2025-11-24 09:17:44 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
+import cli.ConsoleUI;
+import communication.NetworkService;
+import communication.TcpNetworkService;
+import command.*;
+
 public class Main {
+
     public static void main(String[] args) {
 
+        NetworkService networkService = new TcpNetworkService();
+        CommandInterpreter interpreter = new CommandInterpreter();
+
+        ConsoleUI cli = new ConsoleUI(interpreter, networkService);
+
+
+        interpreter.register("listen", new ListenCommand(networkService));
+        interpreter.register("connect", new ConnectCommand(networkService));
+        interpreter.register("disconnect", new DisconnectCommand(networkService));
+        interpreter.register("quit", new QuitCommand());
+
+        cli.run();
     }
 }
